8186fd0e9f23a39551f6587795a429109262da75
Angie Hui Qi <125956661+AngieYHQ@users.noreply.github.com>
2025-07-28 16:26:35 +0800

    Delete #! directory

--- #!/usr/bin/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# 1. Install dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# 2. Launch the app
-streamlit run app.py --server.port 8501 --server.address 0.0.0.0
